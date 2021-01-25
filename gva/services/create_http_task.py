1760d73c7874375c0a5165d8259b4a61b1527e9d
Jonathan <Jonathan.Habte@lloydsbanking.com>
2021-01-25 17:15:47 +0000

    working create_http task but not a fn yet@

--- gva/services/create_http_task.py
+++ gva/services/create_http_task.py
@@ -18,59 +18,77 @@ Adapted From:
 https://github.com/googleapis/python-tasks/blob/master/samples/snippets/create_http_task.py
 """
 
-def create_http_task(
-    project, queue, url, location='europe-west1', payload=None, in_seconds=None, task_name=None, credentials=None
-):
-    # [START cloud_tasks_create_http_task]
-    """Create a task for a given queue with an arbitrary payload."""
+# def create_http_task(
+#     project, queue, url, location='europe-west2', payload=None, in_seconds=None, task_name=None, credentials=None
+# ):
+# [START cloud_tasks_create_http_task]
+"""Create a task for a given queue with an arbitrary payload."""
 
-    from google.cloud import tasks_v2
-    from google.protobuf import timestamp_pb2
-    import datetime
-    import json
-    import gva.logging
+from google.cloud import tasks_v2
+from google.protobuf import timestamp_pb2
+import datetime
+import json
+import gva.logging
+from google.oauth2 import service_account
 
-    logger = gva.logging.get_logger()
+logger = gva.logging.get_logger()
 
-    # Create a client.
-    if credentials:
-        client = tasks_v2.CloudTasksClient(credentials=credentials)
-    else:
-        client = tasks_v2.CloudTasksClient()
+CREDENTIALS_FILE = "jon-deploy-project-150f60b0b3c3.json"
 
-    # Construct the fully qualified queue name.
-    parent = client.queue_path(project, location, queue)
+# credentials = service_account.Credentials.from_service_account_file(
+#     CREDENTIALS_FILE, scopes=["https://www.googleapis.com/auth/cloud-platform"],
+# )
+# credentials=None
+# # Create a client.
+# if credentials:
+#     client = tasks_v2.CloudTasksClient(credentials=credentials)
+# else:
+client = tasks_v2.CloudTasksClient()
 
-    # Construct the request body.
-    task = {
-        "http_request": {  # Specify the type of request.
-            "http_method": tasks_v2.HttpMethod.POST,
-            "url": url,  # The full url path that the task will be sent to.
-        }
+location='europe-west2'
+
+project = 'jon-deploy-project'
+queue = 'my-queue'
+
+# Construct the fully qualified queue name.
+parent = client.queue_path(project, location, queue)
+print(F'the value of parent is {parent}')
+url = 'https://pls-nvd-post-3hipbvdnza-nw.a.run.app/ingest'
+payload = 'Data is ingesting!'
+in_seconds=900
+
+# Construct the request body.
+task = {
+    "http_request": {  # Specify the type of request.
+        "http_method": tasks_v2.HttpMethod.POST,
+        "url": url,  # The full url path that the task will be sent to.
     }
-    if payload is not None:
-        if isinstance(payload, dict):
-            # Convert dict to JSON string
-            payload = json.dumps(payload)
-            # specify http content-type to application/json
-            task["http_request"]["headers"] = {"Content-type": "application/json"}
-        # The API expects a payload of type bytes.
-        converted_payload = payload.encode()
-        task["http_request"]["body"] = converted_payload
-    if in_seconds is not None:
-        # Convert "seconds from now" into an rfc3339 datetime string.
-        d = datetime.datetime.utcnow() + datetime.timedelta(seconds=in_seconds)
-        # Create Timestamp protobuf.
-        timestamp = timestamp_pb2.Timestamp()
-        timestamp.FromDatetime(d)
-        # Add the timestamp to the tasks.
-        task["schedule_time"] = timestamp
-    if task_name is not None:
-        # Add the name to tasks.
-        task["name"] = task_name
+}
+if payload is not None:
+    if isinstance(payload, dict):
+        # Convert dict to JSON string
+        payload = json.dumps(payload)
+        # specify http content-type to application/json
+        task["http_request"]["headers"] = {"Content-type": "application/json"}
+    # The API expects a payload of type bytes.
+    converted_payload = payload.encode()
+    task["http_request"]["body"] = converted_payload
+    print(f'the converted payload is {converted_payload}')
+if in_seconds is not None:
+    # Convert "seconds from now" into an rfc3339 datetime string.
+    d = datetime.datetime.utcnow() + datetime.timedelta(seconds=in_seconds)
+    # Create Timestamp protobuf.
+    timestamp = timestamp_pb2.Timestamp()
+    timestamp.FromDatetime(d)
+    # Add the timestamp to the tasks.
+    task["schedule_time"] = timestamp
+# if task_name is not None:
+#     # Add the name to tasks.
+#     task["name"] = task_name
+# print (f'the task name is {task_name}')
 
-    # Use the client to build and send the task.
-    response = client.create_task(request={"parent": parent, "task": task})
+# Use the client to build and send the task.
+response = client.create_task(request={"parent": parent, "task": task})
 
-    logger.debug(F"[CREATE_TASK] Success - {queue} - {url} - {response.name}")
-    return response
+logger.debug(F"[CREATE_TASK] Success - {queue} - {url} - {response.name}")
+# return response
